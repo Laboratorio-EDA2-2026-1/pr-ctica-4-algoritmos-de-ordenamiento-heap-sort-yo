903daeeb76ce31d80db84a80fd9c63732cef7c7a
JulioCesarSantos21 <cesarsg1829@gmail.com>
2025-09-19 08:29:46 -0600

    Actualizar heapsort.c

--- heapsort.c
+++ heapsort.c
@@ -48,4 +48,70 @@ void build_max_heap(int a[], int n) {
 
 void heap_sort(int a[], int n) {
     /* TODO */
+}#include <stdio.h>
+#include <stdlib.h>
+
+/* PROTOTIPOS: NO CAMBIAR LAS FIRMAS */
+void max_heapify(int a[], int n, int i);
+void build_max_heap(int a[], int n);
+void heap_sort(int a[], int n);
+
+/* MAIN de prueba mínima */
+int main(void) {
+    int n; if (scanf("%d", &n) != 1 || n <= 0) return 0;
+    int *a = (int*)malloc(sizeof(int) * n);
+    if (!a) return 0;
+    for (int i = 0; i < n; ++i) scanf("%d", &a[i]);
+
+    heap_sort(a, n);
+
+    for (int i = 0; i < n; ++i) {
+        if (i) putchar(' ');
+        printf("%d", a[i]);
+    }
+    putchar('\n');
+
+    free(a);
+    return 0;
 }
+
+/* IMPLEMENTAR AQUÍ */
+void max_heapify(int a[], int n, int i) {
+    int largest = i;          // raíz
+    int left = 2 * i + 1;     // hijo izquierdo
+    int right = 2 * i + 2;    // hijo derecho
+
+    if (left < n && a[left] > a[largest])
+        largest = left;
+
+    if (right < n && a[right] > a[largest])
+        largest = right;
+
+    if (largest != i) {
+        int temp = a[i];
+        a[i] = a[largest];
+        a[largest] = temp;
+
+        max_heapify(a, n, largest);
+    }
+}
+
+void build_max_heap(int a[], int n) {
+    // El último nodo no hoja está en n/2 - 1
+    for (int i = n / 2 - 1; i >= 0; i--) {
+        max_heapify(a, n, i);
+    }
+}
+
+void heap_sort(int a[], int n) {
+    build_max_heap(a, n);
+
+    for (int i = n - 1; i > 0; i--) {
+        int temp = a[0];
+        a[0] = a[i];
+        a[i] = temp;
+
+        max_heapify(a, i, 0);
+    }
+}
+
