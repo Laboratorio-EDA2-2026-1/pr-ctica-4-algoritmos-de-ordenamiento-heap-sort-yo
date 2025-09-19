914e97700da593a9fddca2b39fa819fbcfc35f13
JulioCesarSantos21 <cesarsg1829@gmail.com>
2025-09-19 08:50:39 -0600

    Update heapsort.c

--- heapsort.c
+++ heapsort.c
@@ -29,46 +29,7 @@ int main(void) {
     for (int i = 0; i < n; ++i) {
         if (i) putchar(' ');
         printf("%d", a[i]);
-    }
-    putchar('
-');
-
-    free(a);
-    return 0;
-}
-
-/* IMPLEMENTAR AQUÍ */
-void max_heapify(int a[], int n, int i) {
-    /* TODO */
-}
-
-void build_max_heap(int a[], int n) {
-    /* TODO */
-}
-
-void heap_sort(int a[], int n) {
-    /* TODO */
-}#include <stdio.h>
-#include <stdlib.h>
-
-/* PROTOTIPOS: NO CAMBIAR LAS FIRMAS */
-void max_heapify(int a[], int n, int i);
-void build_max_heap(int a[], int n);
-void heap_sort(int a[], int n);
-
-/* MAIN de prueba mínima */
-int main(void) {
-    int n; if (scanf("%d", &n) != 1 || n <= 0) return 0;
-    int *a = (int*)malloc(sizeof(int) * n);
-    if (!a) return 0;
-    for (int i = 0; i < n; ++i) scanf("%d", &a[i]);
-
-    heap_sort(a, n);
-
-    for (int i = 0; i < n; ++i) {
-        if (i) putchar(' ');
-        printf("%d", a[i]);
-    }
+    } 
     putchar('\n');
 
     free(a);
@@ -77,15 +38,18 @@ int main(void) {
 
 /* IMPLEMENTAR AQUÍ */
 void max_heapify(int a[], int n, int i) {
+    /* TODO */
     int largest = i;          // raíz
     int left = 2 * i + 1;     // hijo izquierdo
     int right = 2 * i + 2;    // hijo derecho
 
-    if (left < n && a[left] > a[largest])
+    if (left < n && a[left] > a[largest]){
         largest = left;
+    }
 
-    if (right < n && a[right] > a[largest])
+    if (right < n && a[right] > a[largest]){
         largest = right;
+    }
 
     if (largest != i) {
         int temp = a[i];
@@ -97,16 +61,17 @@ void max_heapify(int a[], int n, int i) {
 }
 
 void build_max_heap(int a[], int n) {
-    // El último nodo no hoja está en n/2 - 1
-    for (int i = n / 2 - 1; i >= 0; i--) {
+    /* TODO */
+    for (int i = n / 2 - 1; i >= 0; --i) {
         max_heapify(a, n, i);
     }
 }
 
 void heap_sort(int a[], int n) {
+    /* TODO */
     build_max_heap(a, n);
 
-    for (int i = n - 1; i > 0; i--) {
+    for (int i = n - 1; i > 0; --i) {
         int temp = a[0];
         a[0] = a[i];
         a[i] = temp;
@@ -114,4 +79,3 @@ void heap_sort(int a[], int n) {
         max_heapify(a, i, 0);
     }
 }
-
